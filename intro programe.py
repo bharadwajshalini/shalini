9df749b254bd1fe1d389fd8c0521cf69bcfa724e
bharadwajshalini <94230640+bharadwajshalini@users.noreply.github.com>
2021-12-18 21:38:42 +0530

    Add files via upload

---
+++ intro programe.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  4 22:31:02 2021
+
+@author: DELL
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 25 16:45:56 2021
+
+@author: pc
+"""
+
+V = [1,2,3,4,5]
+print(V)
+
+import matplotlib.pyplot as plt
+plt.plot(V)
